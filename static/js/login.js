c38260b6e046916c745934a65bb1b85085cb4f50
sycoumba <sycoumba193@gmail..com>
2021-07-07 15:30:47 +0000

    first commit

---
+++ static/js/login.js
@@ -0,0 +1,9 @@
+
+$(".reveal").on('click',function() {
+    var $pwd = $(".pwd");
+    if ($pwd.attr('type') === 'password') {
+        $pwd.attr('type', 'text');
+    } else {
+        $pwd.attr('type', 'password');
+    }
+});
